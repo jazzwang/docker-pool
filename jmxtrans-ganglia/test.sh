e52e03d239ec69c95a581a5d46abbb857a5c0151
jazzwang <jazzwang.tw@gmail.com>
2016-09-13 12:40:36 +0800

    update jmxtrans-ganglia

--- jmxtrans-ganglia/test.sh
+++ jmxtrans-ganglia/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name jmxtrans -h jmxtrans -p 8888:80 -v $(pwd):/var/lib/jmxtrans jazzwang/jmxtrans-ganglia
+docker run -itd --name jmxtrans -h jmxtrans -p 8888:80 -v $(pwd):/var/lib/jmxtrans jazzwang/jmxtrans-ganglia
